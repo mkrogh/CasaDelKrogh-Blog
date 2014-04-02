e9955ca4b4c4a5614be861dee7ffae3ca9cfdc9d
Markus Krogh <markus@casadelkrogh.dk>
2014-04-02 21:46:48 +0200

    New job..

--- _config/deploy.rb
+++ _config/deploy.rb
@@ -4,8 +4,8 @@ set :repository,  "git://github.com/mkrogh/CasaDelKrogh-Blog.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "bia.boundless.dk"                          # Your HTTP server, Apache/etc
-role :app, "bia.boundless.dk"                          # This may be the same as your `Web` server
+role :web, "nyx.boundless.dk"                          # Your HTTP server, Apache/etc
+role :app, "nyx.boundless.dk"                          # This may be the same as your `Web` server
 
 set :deploy_to, "/var/www/#{application}"
 set :use_sudo, false
